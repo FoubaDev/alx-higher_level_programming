389f5fb8e83d3f793bbd3e738bb71e9384d7c3db
foubadev <foubabertrand@gmail.com>
2023-11-29 00:52:02 +0100

    remove h in the word greather

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,11 +4,11 @@ number = random.randint(-10000, 10000)
 while number >= 0:
     last = number % 10
     if last > 6:
-        print(f"Last digit of {number} is {last} and is greather than 5")
+        print(f"Last digit of {number} is {last} and is greater than 5")
     elif last in range(1, 6):
         print(f"Last digit of {number} is {last} and is less than 6 and not 0")
     else:
-        print(f"Last digit of {number} is {last}  and is 0")
+        print(f"Last digit of {number} is {last} and is 0")
     break
 if number < 0:
     last = abs(number) % 10
