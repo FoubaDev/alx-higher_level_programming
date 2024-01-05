057d94e2638ca66f9a15f66c2756182f78dce07a
foubadev <foubabertrand@gmail.com>
2024-01-05 18:20:16 +0100

    file pycodestyle

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
-def copy_list(l):
-    """
-    Makes copy of list
-    Args :
-         l (list): passed list
-    Returns:
-            list copy
-    """
-    return l.copy()
+def copy_list(my_list):
+    return my_list.copy()
