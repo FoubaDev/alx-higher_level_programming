cd1024048904edfbc260fc79d0b73653eb7d17d9
foubadev <foubabertrand@gmail.com>
2024-01-05 18:32:39 +0100

    Write a class LockedClass

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
-class LockedClass:
+"""Write a class LockedClass"""
 
+
+class LockedClass:
+    """
+    Prevents the user from dynamically creating new instance.
+    Attributes:
+            firstname (str): attibute that allows to init
+    """
+
+    __slots__ = ["first_name"]
