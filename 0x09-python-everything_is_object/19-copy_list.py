637b05e297bd1e01752a404bb0f608d0c06bdb85
foubadev <foubabertrand@gmail.com>
2024-01-05 18:06:53 +0100

    python files

---
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def copy_list(l):
+    """
+    Makes copy of list
+    Args :
+         l (list): passed list
+    Returns:
+            list copy
+    """
+    return l.copy()
